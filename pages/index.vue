0891e4487b44fb834ca1192cf702afcfd8a95059
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-05-29 16:58:31 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -19,17 +19,36 @@
         >
           GitHub
         </a>
+        <button @click="recaptcha">Execute recaptcha</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { VueReCaptcha } from 'vue-recaptcha-v3'
 import Logo from '~/components/Logo.vue'
+
+// For more options see below
+Vue.use(VueReCaptcha, { siteKey: '<site key>' })
 
 export default {
   components: {
     Logo
+  },
+  methods: {
+    async recaptcha() {
+      // (optional) Wait until recaptcha has been loaded.
+      await this.$recaptchaLoaded()
+
+      // Execute reCAPTCHA with action "login".
+      const token = await this.$recaptcha('login')
+
+      // Do stuff with the received token.
+      // eslint-disable-next-line no-console
+      console.log(token)
+    }
   }
 }
 </script>
